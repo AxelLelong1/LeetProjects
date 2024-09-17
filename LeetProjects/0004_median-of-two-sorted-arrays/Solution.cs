e2f73f39031d62dd56a7aee77a2b03104acaee8c
axel.lelong <axel.lelong@outlook.com>
2024-09-17 10:24:33 +0200

    0004 Done

---
+++ LeetProjects/0004_median-of-two-sorted-arrays/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetProjects._0004_median_of_two_sorted_arrays
+{
+    /// <summary>
+    /// Class containing the 4th solution of LeetCode problems
+    /// <para>level: Hard</para>
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// Given two sorted arrays nums1 and nums2 of size m and n respectively.
+        /// </summary>
+        /// <param name="nums1"> The first array</param>
+        /// <param name="nums2"> The second array</param>
+        /// <returns>the median of the two sorted arrays</returns>
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] full = nums1.Concat(nums2).ToArray();
+            Array.Sort(full);
+
+            int size = full.Length;
+
+            if (size % 2 == 1)
+                return full[size / 2];
+            else
+                return (full[size / 2] + full[(size / 2) - 1]) / 2.0;
+        }
+
+        public void print(int[] a, int[] b)
+        {
+            Console.WriteLine("Median array of " + a.ToString() + " and " + b.ToString() + " is " + FindMedianSortedArrays(a, b));
+        }
+    }
+}
--- LeetProjects/Program.cs
+++ LeetProjects/Program.cs
@@ -1,5 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using LeetProjects._0004_median_of_two_sorted_arrays;
+
+/*---- 0004 ----*/
+
+Console.WriteLine("4. Median Of Two Sorted Array");
+
+Solution solution = new Solution();
+
+solution.print([1, 3], [2]);
+solution.print([1, 3], [2, 4]);
 
 
-Console.WriteLine("Hello, World!");
+/*---- 0005 ----*/
